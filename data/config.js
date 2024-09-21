46a8359ac43aada4889b059497447996846b5406
aadi219 <aaadibadola@gmail.com>
2024-09-20 20:05:51 -0400

    establish connection to mongoDB

---
+++ data/config.js
@@ -0,0 +1,10 @@
+// config variables for mongoDB
+const config = {
+  user: "admin",
+  password: "password",
+  host: "127.0.0.1",
+  port: 3123,
+  database: "comp3123-assign01",
+};
+
+export default config;
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
+import dbConfig from "./data/config.js";
 
 const app = express();
-const PORT = 8081;
+const PORT = 8082;
 
+const connString = `mongodb://${dbConfig.user}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`;
+// connect to mongoDB
+mongoose
+  .connect(connString, {
+    authSource: "admin",
+  })
+  .then(() => {
+    console.log("Connected to Mongo Database");
+  })
+  .catch(() => {
+    console.err(
+      `[ERR] Error occurred when attempting to connect to MongoDB: `,
+      err,
+    );
+  });
+
+// start server
 const server = app.listen(PORT, () => {
   console.log(`Server is listening on PORT: ${PORT}`);
 });
