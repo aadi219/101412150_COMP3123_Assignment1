6fd67774e4eb95be3625a1d9a38a538edbc7b375
aadi219 <aaadibadola@gmail.com>
2024-09-21 11:03:14 -0400

    fix model timestamp syntax

--- models/Employee.js
+++ models/Employee.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
-const EmployeeSchema = mongoose.Schema({
-  first_name: String,
-  last_name: String,
-  email: String,
-  position: String,
-  date_of_joining: Date,
-  department: String,
-  timestamps: true
-})
+const EmployeeSchema = mongoose.Schema(
+  {
+    first_name: String,
+    last_name: String,
+    email: String,
+    position: String,
+    date_of_joining: Date,
+    department: String,
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const Employee = mongoose.model("Employee", EmployeeSchema);
 export default Employee;
--- models/User.js
+++ models/User.js
@@ -1,22 +1,24 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true,
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  timestamps: true,
-});
+  { timestamps: true },
+);
 
-const User = mongoose.modle("User", UserSchema);
+const User = mongoose.model("User", UserSchema);
 export default User;
