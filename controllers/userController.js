16312237af836b66f4ef983cea2e492151b891bc
aadi219 <aaadibadola@gmail.com>
2024-10-11 08:41:11 -0400

    add password hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,15 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 const signup = async (req, res) => {
   const { username, email, password } = req.body;
-  const user = new User({ username, email, password });
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(password, salt);
+  const user = new User({
+    username: username,
+    email: email,
+    password: hashedPass,
+  });
   await user.save();
   res.status(201).json({
     message: "User created successfully",
@@ -23,9 +30,9 @@ const login = async (req, res) => {
     return res.status(404).json({ error: "Invalid credentials" });
   }
   return res.status(200).json({ message: "Login successful." });
-}
+};
 
 export default {
   signup,
-  login
-}
+  login,
+};
